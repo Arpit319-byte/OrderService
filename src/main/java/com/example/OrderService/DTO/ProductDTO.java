b713bb39a30d1f09623bc8bc52218ea4a1ff0310
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-07-16 20:54:26 +0530

    Added the attributes for the ProductDTO

--- src/main/java/com/example/OrderService/DTO/ProductDTO.java
+++ src/main/java/com/example/OrderService/DTO/ProductDTO.java
@@ -1,4 +1,22 @@
 package com.example.OrderService.DTO;
 
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductDTO {
+
+    Long id;
+    String title;
+    String image;
+    Double price;
+    String description;
+    String brand;
+    String model;
+    boolean popular;
+    double discount;
 }
